163492894f7118ea7d4daa2c822e659016718e63
s33503 <maciejsamoraj00@gmail.com>
2026-03-18 13:00:17 +0100

    init ksiazka

---
+++ src/Ksiazka.java
@@ -0,0 +1,43 @@
+public class Ksiazka {
+    private String tytul;
+    private String autor;
+    private int liczbastron;
+    private boolean dostepna;
+
+    public Ksiazka(String tytul, String autor, int liczbastron, boolean dostepna){
+        this.tytul = tytul;
+        this.autor = autor;
+        this.liczbastron = liczbastron;
+        this.dostepna = dostepna;
+    }
+
+    public void wypiszinfo(){
+        System.out.println("Tytul: "+this.tytul);
+        System.out.println("Autor: "+this.autor);
+        System.out.println("liczba stron: "+this.liczbastron);
+        System.out.println("dostepna: "+this.dostepna);
+    }
+
+    public void wyporzycz(){
+        if(dostepna) {
+            dostepna = false;
+            System.out.println("Ksiazka: "+ tytul +" zostala wyporzycona");
+        }else{
+            System.out.println("Ksiazka: "+ tytul +"nie jest dostepna");
+
+        }
+
+    }
+
+    public void zwroc(){
+        if (!dostepna) {
+            dostepna = true;
+            System.out.println("Ksiazka: "+ tytul +" zostala zwrucona");
+
+        }else{
+            System.out.println("Ksiazka: "+ tytul +" jest dostepna");
+
+        }
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+
+
 public class Main {
     public static void main (String[] args){
+        Ksiazka harrypotter = new Ksiazka("Harry","Rowling", 300, true);
+        Ksiazka blueberry = new Ksiazka("berry", "blue", 150, false);
+        Ksiazka lemonislemon = new Ksiazka("noMoreLemon", "lemonDisguise", 75, true);
+
+
+        harrypotter.wypiszinfo();
+        blueberry.wyporzycz();
+        blueberry.wypiszinfo();
+        lemonislemon.wypiszinfo();
+        lemonislemon.wyporzycz();
+        lemonislemon.wypiszinfo();
+        lemonislemon.zwroc();
+        lemonislemon.wypiszinfo();
 
     }
 }
